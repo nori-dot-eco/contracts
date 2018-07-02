6c8577588d03301d14ef34fd00d4de0d730fbc28
jaycenhorton <jaycen.horton@gmail.com>
2018-07-02 11:51:43 -0700

    rename to RootRegistry

--- contracts/registry/RootRegistryV0_1_0.sol
+++ contracts/registry/RootRegistryV0_1_0.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.4.23;
 import "./ContractRegistryBase.sol";
 
 
-/// @title RegistrysRegistryV0_1_0 
+/// @title RootRegistryV0_1_0 
 /// @dev keeps track of the latest version of the registry
 ///  this registry keeps track of all upgraded registries and their versions
 ///  you can use it to see the full history of registry implementation addresses,
 ///  but should not be used for anything else (including to get the current registry addr)
-contract RegistrysRegistryV0_1_0 is ContractRegistryBase {
+contract RootRegistryV0_1_0 is ContractRegistryBase {
 
   constructor() public ContractRegistryBase() {}
 
