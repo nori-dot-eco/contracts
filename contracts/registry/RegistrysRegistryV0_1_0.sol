985bb02acdcbd42849de3442e3b5f4b5f8072679
jaycenhorton <jaycen.horton@gmail.com>
2018-07-02 11:43:17 -0700

    remove typo from copy pasta

--- contracts/registry/RegistrysRegistryV0_1_0.sol
+++ contracts/registry/RegistrysRegistryV0_1_0.sol
@@ -4,7 +4,7 @@ import "./ContractRegistryBase.sol";
 
 /// @title RegistrysRegistryV0_1_0 
 /// @dev keeps track of the latest version of the registry
-///  this non upgradeable registry keeps track of all upgraded registries and their versions
+///  this registry keeps track of all upgraded registries and their versions
 ///  you can use it to see the full history of registry implementation addresses,
 ///  but should not be used for anything else (including to get the current registry addr)
 contract RegistrysRegistryV0_1_0 is ContractRegistryBase {
