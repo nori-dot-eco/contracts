fd2c94f897bb8fd6efe89272426e6f447ac69ddf
jaycenhorton <jaycen.horton@gmail.com>
2018-07-02 14:47:35 -0700

    make it so tests only run via 0_setup_tests

--- test/EIP820.test.js
+++ test/EIP820.test.js
@@ -6,31 +6,36 @@ const FifoCrcMarket = artifacts.require('./FifoCrcMarket.sol');
 let registry;
 let fifoCrcMarket;
 let web3;
-
-before(async () => {
-  registry = await EIP820Registry.deployed();
-  fifoCrcMarket = await FifoCrcMarket.deployed();
-  web3 = await new Web3();
-});
-// todo jaycen i dont think these tests are accurate (see multisig 820 tests for better examples?)
-contract('FifoCrcMarket', accounts => {
-  describe('Check if FifoCrcMarket can implement ERC820', () => {
-    it('should return a valid matching address', async () => {
-      const interfaceHash = await registry.interfaceHash(
-        'EIP777TokensRecipient'
-      );
-      assert.equal(interfaceHash, web3.sha3('EIP777TokensRecipient'));
-      await registry.setInterfaceImplementer(
-        accounts[0],
-        interfaceHash,
-        fifoCrcMarket.address,
-        { from: accounts[0] }
-      );
-      const rImplementer = await registry.getInterfaceImplementer(
-        accounts[0],
-        interfaceHash
-      );
-      assert.equal(rImplementer, fifoCrcMarket.address);
+const EIP820RegistryTests = () => {
+  before(async () => {
+    registry = await EIP820Registry.deployed();
+    fifoCrcMarket = await FifoCrcMarket.deployed();
+    web3 = await new Web3();
+  });
+  // todo jaycen i dont think these tests are accurate (see multisig 820 tests for better examples?)
+  contract('EIP820Registry', accounts => {
+    describe('Check if FifoCrcMarket can implement ERC820', () => {
+      it('should return a valid matching address', async () => {
+        const interfaceHash = await registry.interfaceHash(
+          'EIP777TokensRecipient'
+        );
+        assert.equal(interfaceHash, web3.sha3('EIP777TokensRecipient'));
+        await registry.setInterfaceImplementer(
+          accounts[0],
+          interfaceHash,
+          fifoCrcMarket.address,
+          { from: accounts[0] }
+        );
+        const rImplementer = await registry.getInterfaceImplementer(
+          accounts[0],
+          interfaceHash
+        );
+        assert.equal(rImplementer, fifoCrcMarket.address);
+      });
     });
   });
-});
+};
+
+module.exports = {
+  EIP820RegistryTests,
+};
