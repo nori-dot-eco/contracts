6f8d55688ec0af54847a7f5774fd4cd9e069408f
jaycenhorton <jaycen.horton@gmail.com>
2018-07-12 11:20:12 -0700

    reenable test migration

--- migrations/4_deploy_test_contracts.js
+++ migrations/4_deploy_test_contracts.js
@@ -11,29 +11,31 @@ const SelectableCrcMarketV0_1_0 = artifacts.require(
 module.exports = function deploy(deployer, network, accounts) {
   deployer.then(async () => {
     // yeah we won't be using this...
-    // if (network !== 'test') {
-    //   return;
-    // }
-    // const rootRegistry = await RootRegistryV0_1_0.deployed();
-    // const registry = ContractRegistryV0_1_0.at(
-    //   await rootRegistry.getLatestProxyAddr.call('ContractRegistry')
-    // );
-    // await deployer.deploy(EIP820Registry);
-    // const crcMarket = await deployer.deploy(SelectableCrcMarketV0_1_0);
-    // await crcMarket.initialize(
-    //   registry.address,
-    //   [
-    //     await registry.getLatestProxyAddr.call('CRC'),
-    //     await registry.getLatestProxyAddr.call('Nori'),
-    //   ],
-    //   accounts[0]
-    // );
-    // // todo only deploy this  to main net with nori mainnet addresses as owners
-    // await deployer.deploy(
-    //   MultiSigWallet,
-    //   [0x1d75abdf70d84e3bec66d3ce60145dddca3bcc06],
-    //   web3.toBigNumber(1),
-    //   registry.address
-    // );
+    if (network !== 'test') {
+      return;
+    }
+    const rootRegistry = await RootRegistryV0_1_0.deployed();
+    const registry = ContractRegistryV0_1_0.at(
+      await rootRegistry.getLatestProxyAddr.call('ContractRegistry')
+    );
+    await deployer.deploy(EIP820Registry);
+
+    const crcMarket = await deployer.deploy(SelectableCrcMarketV0_1_0);
+    await crcMarket.initialize(
+      registry.address,
+      [
+        await registry.getLatestProxyAddr.call('CRC'),
+        await registry.getLatestProxyAddr.call('Nori'),
+      ],
+      accounts[0]
+    );
+
+    // todo only deploy this  to main net with nori mainnet addresses as owners
+    await deployer.deploy(
+      MultiSigWallet,
+      [0x1d75abdf70d84e3bec66d3ce60145dddca3bcc06],
+      web3.toBigNumber(1),
+      registry.address
+    );
   });
 };
