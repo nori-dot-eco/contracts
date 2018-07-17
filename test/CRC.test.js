ae7754d846f624b5fb8a3874c0f7940db91a88eb
jaycenhorton <jaycen.horton@gmail.com>
2018-07-16 20:27:53 -0700

    splitting pr

--- test/CRC.test.js
+++ test/CRC.test.js
@@ -1,8 +1,8 @@
 import { shouldBehaveLikeCrc } from './behaviors/Crc';
-import { testBasicCommodityFunctions } from './behaviors/BasicCommodity';
+// import { testBasicCommodityFunctions } from './behaviors/BasicCommodity';
 
 const CRCTests = admin => {
   shouldBehaveLikeCrc(admin);
-  testBasicCommodityFunctions();
+  // testBasicCommodityFunctions(); // enabled in next pr
 };
 export default CRCTests;
