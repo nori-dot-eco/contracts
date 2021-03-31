f5267302f24aa3f779fc7db6d83379353545a0d1
jaycenhorton <jaycen.horton@gmail.com>
2021-03-31 13:34:01 -0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,3 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  env: {
-    browser: true,
-    node: true,
-    mocha: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 8,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  globals: {
-    contract: false,
-    assert: false,
-    web3: true,
-    artifacts: true,
-  },
-  rules: {
-    camelcase: 'off',
-    'no-plusplus': 'off',
-    'one-var': 'off',
-    'prefer-destructuring': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: true,
-        peerDependencies: true,
-      },
-    ],
-  },
+  extends: '@nori-dot-com/eslint-config-nori',
 };
