4ef6f51084c642af1219bf4ea8b67843c185d853
jaycenhorton <jaycen.horton@gmail.com>
2021-07-29 21:16:20 -0700

    add transferWithFee function to nori token

--- contracts/Nori_V0.sol
+++ contracts/Nori_V0.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mint
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract Nori_V0 is Initializable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
+  event TransferWithFee(address indexed from, address indexed to, uint256 indexed value, address feeAddress, uint256 fee);
 
   function initialize() public initializer {
     ERC20Detailed.initialize("Nori", "NORI", 18);
@@ -14,5 +15,23 @@ contract Nori_V0 is Initializable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
     _removePauser(address(this));
     _addMinter(msg.sender);
     _addPauser(msg.sender);
+  }
+
+  function transferWithFee(
+    address _to,
+    uint256 _amount,
+    address _feeAddress,
+    uint256 _fee
+  ) public whenNotPaused returns (bool) {
+    super.transfer(_feeAddress, _fee);
+    super.transfer(_to, _amount);
+    emit TransferWithFee(
+      msg.sender,
+      _to,
+      _amount,
+      _feeAddress,
+      _fee
+    );
+    return true;
   }
 }
