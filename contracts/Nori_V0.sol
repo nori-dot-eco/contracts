8103934a812d36704df3f7cbed2cf6453ca6fe6f
Jaycen Horton <jaycen.horton@gmail.com>
2021-08-11 11:04:17 -0700

    add transferWithFee function to nori token (#123)
    * add transferWithFee function to nori token
    
    * add hardhat
    
    * add hardhat
    
    * update ganache
    
    * bump circle ci node version
    
    * update lockfile

--- contracts/Nori_V0.sol
+++ contracts/Nori_V0.sol
@@ -5,6 +5,14 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mint
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract Nori_V0 is Initializable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
+  event TransferWithFeeAndMemo(
+    address indexed from,
+    address indexed to,
+    uint256 indexed value,
+    address feeAddress,
+    uint256 fee,
+    bytes32 memo
+  );
 
   function initialize() public initializer {
     ERC20Detailed.initialize("Nori", "NORI", 18);
@@ -14,5 +22,25 @@ contract Nori_V0 is Initializable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
     _removePauser(address(this));
     _addMinter(msg.sender);
     _addPauser(msg.sender);
+  }
+
+  function transferWithFeeAndMemo(
+    address _to,
+    uint256 _amount,
+    address _feeAddress,
+    uint256 _fee,
+    bytes32 _memo
+  ) public whenNotPaused returns (bool) {
+    super.transfer(_feeAddress, _fee);
+    super.transfer(_to, _amount);
+    emit TransferWithFeeAndMemo(
+      msg.sender,
+      _to,
+      _amount,
+      _feeAddress,
+      _fee,
+      _memo
+    );
+    return true;
   }
 }
