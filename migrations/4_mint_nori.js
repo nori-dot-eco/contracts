8938868d602ac4d3750a2dad825e1d86c0054d60
amiecorso <amie@nori.com>
2021-09-22 17:04:10 -0700

    formatUnits

--- migrations/4_mint_nori.js
+++ migrations/4_mint_nori.js
@@ -1,10 +1,11 @@
+import { formatUnits } from "@ethersproject/units";
 const Nori_V0 = artifacts.require("Nori_V0");
 
-module.exports = async function(deployer, network, accounts) {
-  if (network === 'test' || network === 'develop') {
+module.exports = async function (deployer, network, accounts) {
+  if (network === "test" || network === "develop") {
     const noriInstance = await Nori_V0.deployed();
-    await noriInstance.mint(accounts[0], '100000000000000000000');
-    await noriInstance.mint(accounts[1], '100000000000000000000');
-    await noriInstance.mint(accounts[2], '100000000000000000000');
+    await noriInstance.mint(accounts[0], formatUnits(100));
+    await noriInstance.mint(accounts[1], formatUnits(100));
+    await noriInstance.mint(accounts[2], formatUnits(100));
   }
 };
