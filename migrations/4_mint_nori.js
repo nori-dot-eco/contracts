2a932d115d6b59e625ed96ad40a1f344afe02eb3
amiecorso <amie@nori.com>
2021-09-23 15:08:37 -0700

    fix formatUnits

--- migrations/4_mint_nori.js
+++ migrations/4_mint_nori.js
@@ -1,11 +1,13 @@
-import { formatUnits } from "@ethersproject/units";
+const { parseUnits } = require("@ethersproject/units");
 const Nori_V0 = artifacts.require("Nori_V0");
 
 module.exports = async function (deployer, network, accounts) {
   if (network === "test" || network === "develop") {
     const noriInstance = await Nori_V0.deployed();
-    await noriInstance.mint(accounts[0], formatUnits(100));
-    await noriInstance.mint(accounts[1], formatUnits(100));
-    await noriInstance.mint(accounts[2], formatUnits(100));
+    const tokensToMint = parseUnits("100");
+
+    await noriInstance.mint(accounts[0], tokensToMint);
+    await noriInstance.mint(accounts[1], tokensToMint);
+    await noriInstance.mint(accounts[2], tokensToMint);
   }
 };
