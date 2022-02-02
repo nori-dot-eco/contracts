7bf395c1af330ef950dfe7a4a7577fddb097e143
amiecorso <amie@nori.com>
2022-02-02 12:50:11 -0800

    fix linter

--- contracts/FIFOMarket.sol
+++ contracts/FIFOMarket.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/utils/introspection/IERC1820RegistryUpgradeable.sol';
-import './Removal.sol';
-import './Certificate.sol';
-import './NORI.sol';
-import 'hardhat/console.sol'; // todo
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/IERC1820RegistryUpgradeable.sol";
+import "./Removal.sol";
+import "./Certificate.sol";
+import "./NORI.sol";
+import "hardhat/console.sol"; // todo
 
 // todo emit events
 
@@ -23,163 +23,163 @@ contract FIFOMarket is
     ERC1155HolderUpgradeable,
     IERC777RecipientUpgradeable
 {
-    IERC1820RegistryUpgradeable private _erc1820;
-    Removal private _removal;
-    Certificate private _certificate;
-    NORI private _nori;
-    mapping(uint256 => uint256) private _queue;
-    uint256 private _queueHeadIndex;
-    uint256 private _queueNextInsertIndex;
-    address private _noriFeeWallet;
-    uint256 private _noriFee;
+  IERC1820RegistryUpgradeable private _erc1820;
+  Removal private _removal;
+  Certificate private _certificate;
+  NORI private _nori;
+  mapping(uint256 => uint256) private _queue;
+  uint256 private _queueHeadIndex;
+  uint256 private _queueNextInsertIndex;
+  address private _noriFeeWallet;
+  uint256 private _noriFee;
 
-    function initialize(
-        address removalAddress,
-        address noriAddress,
-        address certificateAddress,
-        address noriFeeWalletAddress,
-        uint256 noriFee
-    ) public initializer {
-        __Context_init_unchained();
-        __ERC165_init_unchained();
-        __AccessControl_init_unchained();
-        __AccessControlEnumerable_init_unchained();
-        __ERC1155Receiver_init_unchained();
-        _removal = Removal(removalAddress);
-        _nori = NORI(noriAddress);
-        _certificate = Certificate(certificateAddress);
-        _noriFeeWallet = noriFeeWalletAddress;
-        _noriFee = noriFee;
-        _erc1820 = IERC1820RegistryUpgradeable(
-            0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
-        ); // todo
-        _erc1820.setInterfaceImplementer(
-            address(this),
-            keccak256('ERC777TokensRecipient'),
-            address(this)
-        );
-        _queueHeadIndex = 0;
-        _queueNextInsertIndex = 0;
+  function initialize(
+    address removalAddress,
+    address noriAddress,
+    address certificateAddress,
+    address noriFeeWalletAddress,
+    uint256 noriFee
+  ) public initializer {
+    __Context_init_unchained();
+    __ERC165_init_unchained();
+    __AccessControl_init_unchained();
+    __AccessControlEnumerable_init_unchained();
+    __ERC1155Receiver_init_unchained();
+    _removal = Removal(removalAddress);
+    _nori = NORI(noriAddress);
+    _certificate = Certificate(certificateAddress);
+    _noriFeeWallet = noriFeeWalletAddress;
+    _noriFee = noriFee;
+    _erc1820 = IERC1820RegistryUpgradeable(
+      0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
+    ); // todo
+    _erc1820.setInterfaceImplementer(
+        address(this),
+        keccak256("ERC777TokensRecipient"),
+        address(this)
+    );
+    _queueHeadIndex = 0;
+    _queueNextInsertIndex = 0;
+  }
+
+  function _queueLength() private view returns (uint256) {
+    return _queueNextInsertIndex - _queueHeadIndex;
+  }
+
+  function numberOfTonnesInQueue() public view returns (uint256) {
+    uint256 tonnesInQueue = 0;
+    for (uint256 i = _queueHeadIndex; i < _queueNextInsertIndex; i++) {
+      tonnesInQueue += _removal.balanceOf(address(this), _queue[i]);
     }
+    return tonnesInQueue;
+  }
 
-    function _queueLength() private view returns (uint256) {
-        return _queueNextInsertIndex - _queueHeadIndex;
+  function onERC1155BatchReceived(
+    address,
+    address,
+    uint256[] memory ids,
+    uint256[] memory,
+    bytes memory
+  ) public override returns (bytes4) {
+    for (uint256 i = 0; i < ids.length; i++) {
+      _queue[_queueNextInsertIndex] = (ids[i]);
+      _queueNextInsertIndex++;
     }
+    return this.onERC1155BatchReceived.selector;
+  }
 
-    function numberOfTonnesInQueue() public view returns (uint256) {
-        uint256 tonnesInQueue = 0;
-        for (uint256 i = _queueHeadIndex; i < _queueNextInsertIndex; i++) {
-            tonnesInQueue += _removal.balanceOf(address(this), _queue[i]);
+  // todo optimize gas (perhaps consider setting the last sold id instead of looping -- not sure if it's possible to reduce array size yet or not)
+  /**
+    * @dev Called automatically by the ERC777 (nori) contract when a batch of tokens are transferred to the contract.
+    */
+  function tokensReceived(
+    address,
+    address,
+    address,
+    uint256 amount,
+    bytes calldata userData,
+    bytes calldata
+  ) external override {
+    uint256 certificateAmount = (amount * 100) / (100 + _noriFee);
+    uint256 remainingAmountToFill = certificateAmount;
+
+    address recipient = abi.decode(userData, (address));
+    require(recipient == address(recipient), "FIFOMarket: Invalid address");
+    require(
+      recipient != address(0),
+      "FIFOMarket: Cannot mint to the 0 address"
+    );
+    require(
+      msg.sender == address(_nori),
+      "FIFOMarket: This contract can only receive NORI"
+    ); // todo verify this can only be invoked by the nori contract
+    uint256[] memory ids = new uint256[](_queueLength());
+    uint256[] memory amounts = new uint256[](_queueLength());
+    address[] memory suppliers = new address[](_queueLength());
+    for (uint256 i = _queueHeadIndex; i < _queueNextInsertIndex; i++) {
+      uint256 removalAmount = _removal.balanceOf(
+        address(this),
+        _queue[i]
+      );
+      address supplier = _removal.vintage(_queue[i]).supplier;
+      if (remainingAmountToFill < removalAmount) {
+        ids[i] = _queue[i];
+        amounts[i] = remainingAmountToFill;
+        suppliers[i] = supplier;
+        remainingAmountToFill = 0;
+      } else if (remainingAmountToFill >= removalAmount) {
+        if (
+            i == _queueNextInsertIndex - 1 &&
+            remainingAmountToFill > removalAmount
+        ) {
+          revert("FIFOMarket: Not enough supply");
         }
-        return tonnesInQueue;
+        ids[i] = _queue[i];
+        amounts[i] = removalAmount;
+        suppliers[i] = supplier;
+        remainingAmountToFill -= removalAmount;
+      } else {
+        revert("FIFOMarket: Not enough supply");
+      }
+      if (remainingAmountToFill == 0) {
+        break;
+      }
     }
+    // In a completely empty queue, the for loop never runs.
+    // Catch this case here without the gas-heavy numberOfTonnesInQueue call.
+    require(amounts[0] > 0, "FIFOMarket: Not enough supply");
 
-    function onERC1155BatchReceived(
-        address operator,
-        address from,
-        uint256[] memory ids,
-        uint256[] memory values,
-        bytes memory data
-    ) public override returns (bytes4) {
-        for (uint256 i = 0; i < ids.length; i++) {
-            _queue[_queueNextInsertIndex] = (ids[i]);
-            _queueNextInsertIndex++;
-        }
-        return this.onERC1155BatchReceived.selector;
+    bytes memory encodedCertificateAmount = abi.encode(certificateAmount);
+    _certificate.mintBatch(
+        recipient,
+        ids,
+        amounts,
+        encodedCertificateAmount
+    );
+    for (uint256 i = 0; i < ids.length; i++) {
+      if (amounts[i] == 0) {
+        break;
+      }
+      if (amounts[i] == _removal.balanceOf(address(this), _queue[i])) {
+        _queueHeadIndex++;
+      }
+      uint256 noriFee = (amounts[i] / 100) * _noriFee;
+      uint256 supplierFee = amounts[i];
+      _nori.transfer(_noriFeeWallet, noriFee);
+      _nori.transfer(suppliers[i], supplierFee);
     }
+    _removal.burnBatch(address(this), ids, amounts);
+  }
 
-    // todo optimize gas (perhaps consider setting the last sold id instead of looping -- not sure if it's possible to reduce array size yet or not)
-    /**
-     * @dev Called automatically by the ERC777 (nori) contract when a batch of tokens are transferred to the contract.
-     */
-    function tokensReceived(
-        address operator,
-        address from,
-        address to,
-        uint256 amount,
-        bytes calldata userData,
-        bytes calldata operatorData
-    ) external override {
-        uint256 certificateAmount = (amount * 100) / (100 + _noriFee);
-        uint256 remainingAmountToFill = certificateAmount;
-
-        address recipient = abi.decode(userData, (address));
-        require(recipient == address(recipient), 'FIFOMarket: Invalid address');
-        require(
-            recipient != address(0),
-            'FIFOMarket: Cannot mint to the 0 address'
-        );
-        require(
-            msg.sender == address(_nori),
-            'FIFOMarket: This contract can only receive NORI'
-        ); // todo verify this can only be invoked by the nori contract
-        uint256[] memory ids = new uint256[](_queueLength());
-        uint256[] memory amounts = new uint256[](_queueLength());
-        address[] memory suppliers = new address[](_queueLength());
-        for (uint256 i = _queueHeadIndex; i < _queueNextInsertIndex; i++) {
-            uint256 removalAmount = _removal.balanceOf(
-                address(this),
-                _queue[i]
-            );
-            address supplier = _removal.vintage(_queue[i]).supplier;
-            if (remainingAmountToFill < removalAmount) {
-                ids[i] = _queue[i];
-                amounts[i] = remainingAmountToFill;
-                suppliers[i] = supplier;
-                remainingAmountToFill = 0;
-            } else if (remainingAmountToFill >= removalAmount) {
-                if (
-                    i == _queueNextInsertIndex - 1 &&
-                    remainingAmountToFill > removalAmount
-                ) {
-                    revert('FIFOMarket: Not enough supply');
-                }
-                ids[i] = _queue[i];
-                amounts[i] = removalAmount;
-                suppliers[i] = supplier;
-                remainingAmountToFill -= removalAmount;
-            } else {
-                revert('FIFOMarket: Not enough supply');
-            }
-            if (remainingAmountToFill == 0) {
-                break;
-            }
-        }
-        // In a completely empty queue, the for loop never runs.
-        // Catch this case here without the gas-heavy numberOfTonnesInQueue call.
-        require(amounts[0] > 0, 'FIFOMarket: Not enough supply');
-
-        bytes memory encodedCertificateAmount = abi.encode(certificateAmount);
-        _certificate.mintBatch(
-            recipient,
-            ids,
-            amounts,
-            encodedCertificateAmount
-        );
-        for (uint256 i = 0; i < ids.length; i++) {
-            if (amounts[i] == 0) {
-                break;
-            }
-            if (amounts[i] == _removal.balanceOf(address(this), _queue[i])) {
-                _queueHeadIndex++;
-            }
-            uint256 noriFee = (amounts[i] / 100) * _noriFee;
-            uint256 supplierFee = amounts[i];
-            _nori.transfer(_noriFeeWallet, noriFee);
-            _nori.transfer(suppliers[i], supplierFee);
-        }
-        _removal.burnBatch(address(this), ids, amounts);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(AccessControlEnumerableUpgradeable, ERC1155ReceiverUpgradeable)
-        returns (bool)
-    {
-        return
-            AccessControlEnumerableUpgradeable.supportsInterface(interfaceId) ||
-            ERC1155ReceiverUpgradeable.supportsInterface(interfaceId);
-    }
+  function supportsInterface(bytes4 interfaceId)
+      public
+      view
+      virtual
+      override(AccessControlEnumerableUpgradeable, ERC1155ReceiverUpgradeable)
+      returns (bool)
+  {
+    return
+      AccessControlEnumerableUpgradeable.supportsInterface(interfaceId) ||
+      ERC1155ReceiverUpgradeable.supportsInterface(interfaceId);
+  }
 }
