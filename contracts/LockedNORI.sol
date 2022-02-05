e6202a92ca89d23c55fd5041958b4fa4f1c37d6f
jaycenhorton <jaycen.horton@gmail.com>
2022-02-04 17:42:47 -0800

    try coveralls

--- contracts/LockedNORI.sol
+++ contracts/LockedNORI.sol
@@ -571,7 +571,7 @@ contract LockedNORI is
 
   function testCoverage(uint256 branch) public pure virtual returns (bool) {
     // @prettier:ignore
-    if (branch == 3 || branch == 4) if(branch ==3) return true; return false;
+    if (branch == 3 || branch == 4) {if(branch ==3) return true;}
     if (branch == 1 || branch == 2) {
       bool brancher = false;
       if (branch == 1) {
