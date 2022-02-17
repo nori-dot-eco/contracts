c5ccee368dc046ee8b817f0bdc35754ed428a9bb
jaycenhorton <jaycen.horton@gmail.com>
2022-02-17 14:25:01 -0800

    fix gas reporter ci

--- config/gas-reporter.ts
+++ config/gas-reporter.ts
@@ -3,8 +3,10 @@ import type { HardhatUserConfig } from 'hardhat/types';
 const REPORT_GAS =
   Boolean(process.env.REPORT_GAS) && process.env.REPORT_GAS !== 'false';
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const REPORT_GAS_FILE = process.env.REPORT_GAS ? process.env.REPORT_GAS_FILE || 'gasReporterOutput.json' : undefined;
-console.log({REPORT_GAS_FILE,REPORT_GAS})
+const REPORT_GAS_FILE = process.env.REPORT_GAS
+  ? process.env.REPORT_GAS_FILE || 'gasReporterOutput.json'
+  : undefined;
+
 export const gasReporter: HardhatUserConfig['gasReporter'] = {
   enabled: REPORT_GAS,
   currency: 'USD',
