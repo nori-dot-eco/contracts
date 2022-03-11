bc9e38cb4b905be320738e55e4738c6e4d54e1d3
scottynomad <scott@nori.com>
2022-03-11 12:45:26 -0800

    Updated solidity pin to 0.8.12

--- contracts/NORI.sol
+++ contracts/NORI.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.10;
+pragma solidity =0.8.12;
 
 import "./ERC777PresetPausablePermissioned.sol";
 
--- contracts/ScheduleUtils.sol
+++ contracts/ScheduleUtils.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.10;
+pragma solidity =0.8.12;
 
 import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 
