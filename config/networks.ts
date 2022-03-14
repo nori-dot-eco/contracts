0d4e61919d7d87fa4acfeb27843cd173632cdcd7
jaycenhorton <jaycen.horton@gmail.com>
2022-03-14 16:38:56 -0700

    fix network config

--- config/networks.ts
+++ config/networks.ts
@@ -46,8 +46,7 @@ const mainnet: NetworkUserConfig = {
 
 export const networks = {
   hardhat,
-  localhost,
-  ...(STAGING_MNEMONIC && { goerli, mumbai }),
+  ...(STAGING_MNEMONIC && { localhost }),
   ...(INFURA_STAGING_KEY && STAGING_MNEMONIC && { goerli, mumbai }),
   ...(INFURA_STAGING_KEY && PROD_MNEMONIC && { mainnet, polygon }),
 } as const;
