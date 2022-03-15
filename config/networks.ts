f895ebfc7950ffc83ee7bbadb861aee56588e060
Jaycen Horton <jaycen.horton@gmail.com>
2022-03-15 15:15:26 -0700

    use MNEMONIC env variable name instead (#197)

--- config/networks.ts
+++ config/networks.ts
@@ -2,7 +2,7 @@ import type { NetworksUserConfig, NetworkUserConfig } from 'hardhat/types';
 
 import { accounts } from '@/config/accounts';
 
-const { INFURA_STAGING_KEY, STAGING_MNEMONIC, PROD_MNEMONIC } = process.env;
+const { INFURA_STAGING_KEY, MNEMONIC } = process.env;
 
 const hardhat: NetworksUserConfig['hardhat'] = {
   blockGasLimit: 20_000_000,
@@ -17,36 +17,40 @@ const localhost: NetworkUserConfig = {
   initialBaseFeePerGas: 1,
   gasPrice: 1,
   chainId: 9001,
-  accounts: { mnemonic: STAGING_MNEMONIC },
+  accounts: { mnemonic: MNEMONIC },
 };
 
 const goerli: NetworkUserConfig = {
   chainId: 5,
   url: `https://goerli.infura.io/v3/${INFURA_STAGING_KEY}`,
-  accounts: { mnemonic: STAGING_MNEMONIC },
+  accounts: { mnemonic: MNEMONIC },
   gas: 2100000,
   gasPrice: 8000000000,
 };
 
 const mumbai: NetworkUserConfig = {
   url: `https://polygon-mumbai.infura.io/v3/${INFURA_STAGING_KEY}`,
-  accounts: { mnemonic: STAGING_MNEMONIC },
+  accounts: { mnemonic: MNEMONIC },
   gasPrice: 35000000000,
 };
 
 const polygon: NetworkUserConfig = {
   url: `https://polygon.infura.io/v3/${INFURA_STAGING_KEY}`, // todo use prod key
-  accounts: { mnemonic: PROD_MNEMONIC },
+  accounts: {
+    mnemonic: MNEMONIC, // todo require fireblocks
+  },
 };
 
 const mainnet: NetworkUserConfig = {
   url: `https://mainnet.infura.io/v3/${INFURA_STAGING_KEY}`, // todo use prod key
-  accounts: { mnemonic: PROD_MNEMONIC },
+  accounts: {
+    mnemonic: MNEMONIC, // todo require fireblocks
+  },
 };
 
 export const networks = {
   hardhat,
-  ...(STAGING_MNEMONIC && { localhost }),
-  ...(INFURA_STAGING_KEY && STAGING_MNEMONIC && { goerli, mumbai }),
-  ...(INFURA_STAGING_KEY && PROD_MNEMONIC && { mainnet, polygon }),
+  ...(MNEMONIC && { localhost }),
+  ...(INFURA_STAGING_KEY && MNEMONIC && { goerli, mumbai }),
+  ...(INFURA_STAGING_KEY && MNEMONIC && { mainnet, polygon }),
 } as const;
