2c52f7128d800e1a37722ee626e0903e19367535
jaycenhorton <jaycen.horton@gmail.com>
2022-03-16 15:32:46 -0700

    add vesting and test task overrides

---
+++ tasks/test.ts
@@ -0,0 +1,35 @@
+import { task } from 'hardhat/config';
+import type { RunSuperFunction } from 'hardhat/types';
+import { TASK_TEST } from 'hardhat/builtin-tasks/task-names';
+
+import { validateTestEnvironment } from '@/tasks/utils/validate-environment';
+
+interface TestTaskOverrideParameters {
+  reportGas: boolean;
+}
+
+export const TASK = {
+  name: TASK_TEST,
+  description: 'Runs mocha tests',
+  run: async (
+    taskArgs: TestTaskOverrideParameters | Record<string, unknown>,
+    hre: CustomHardHatRuntimeEnvironment,
+    runSuper: RunSuperFunction<typeof taskArgs>
+  ) => {
+    const { REPORT_GAS } = process.env;
+    validateTestEnvironment();
+    if (Boolean(taskArgs?.reportGas) || REPORT_GAS === 'true') {
+      hre.log(
+        'Setting process.env.REPORT_GAS to true',
+        `Previous value: ${REPORT_GAS}`
+      );
+      process.env.REPORT_GAS = 'true';
+    }
+    return runSuper(taskArgs);
+  },
+} as const;
+
+task(TASK.name, TASK.description, TASK.run).addFlag(
+  'reportGas',
+  'Run tests and report gas usage'
+);
