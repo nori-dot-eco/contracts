a110d6baecc92005c0e5386e8bf5a2f88d9c0e65
jaycenhorton <jaycen.horton@gmail.com>
2022-03-17 11:59:30 -0700

    fix env overrides

--- config/environment.ts
+++ config/environment.ts
@@ -1,11 +1,14 @@
 import dotenvParseVariables from 'dotenv-parse-variables';
-import dotenv from 'dotenv-defaults';
+import dotenvDefaults from 'dotenv-defaults';
+import dotenv from 'dotenv';
 
 const env = dotenv.config();
+const defaults = dotenvDefaults.config();
 
 if (env.error != null || env.parsed == null) throw env.error;
 
 process.env = {
+  ...defaults,
   ...env,
   ...dotenvParseVariables(env.parsed),
 } as typeof global.process.env;
