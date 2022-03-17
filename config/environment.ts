e338f6c6b7545e46906849d8af7a43885e45d910
jaycenhorton <jaycen.horton@gmail.com>
2022-03-17 13:03:28 -0700

    fix tests

--- config/environment.ts
+++ config/environment.ts
@@ -1,14 +1,16 @@
-import dotenvParseVariables from 'dotenv-parse-variables';
 import dotenvDefaults from 'dotenv-defaults';
-import dotenv from 'dotenv';
-
-const env = dotenv.config();
+import dotenvParseVariables from 'dotenv-parse-variables';
 const defaults = dotenvDefaults.config();
 
-if (env.error != null || env.parsed == null) throw env.error;
+if (defaults.error != null || defaults.parsed == null) throw defaults.error;
 
-process.env = {
-  ...defaults,
-  ...env,
-  ...dotenvParseVariables(env.parsed),
-} as typeof global.process.env;
+process.env = Object.fromEntries(
+  Object.entries(
+    dotenvParseVariables({
+      ...defaults.parsed,
+      ...process.env,
+    } as any)
+  ).map(([k, v]) => [k, (v as string)?.toLowerCase?.() === 'null' ? null : v])
+) as typeof global.process.env;
+
+console.log({ env: process.env });
