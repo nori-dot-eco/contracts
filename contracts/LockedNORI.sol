3704d76dad8890fcaff43e1ec926c3aa4b840dba
scottynomad <scott@nori.com>
2022-04-07 12:48:06 -0400

    Added an extra validation check at the top of revoke to ensure it's not getting called with both amount and time

--- contracts/LockedNORI.sol
+++ contracts/LockedNORI.sol
@@ -600,6 +600,8 @@ contract LockedNORI is
     uint256 atTime,
     uint256 amount
   ) internal {
+    require((atTime == 0 && amount > 0) || (atTime > 0 && amount == 0),
+      "lNORI: Must specify a revocation time or aan amount not both");
     TokenGrant storage grant = _grants[from];
     require(grant.exists, "lNORI: no grant exists");
     require(
