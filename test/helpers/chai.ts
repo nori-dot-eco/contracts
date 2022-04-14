3a075a1b9e747b34fd8617bd6a5344ba42749d63
amiecorso <amie@nori.com>
2022-04-14 16:01:52 -0700

    chai type

--- test/helpers/chai.ts
+++ test/helpers/chai.ts
@@ -3,6 +3,8 @@ import sinonModule from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
 
-export const chai = chaiModule.use(sinonChai).use(chaiAsPromised);
+export const chai: Chai.ChaiStatic = chaiModule
+  .use(sinonChai)
+  .use(chaiAsPromised);
 export const expect = chai.expect;
 export const sinon = sinonModule;
