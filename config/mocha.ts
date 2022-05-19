0fb82b67a35fd7f133df8918a544c417b8d65fc2
jaycenhorton <jaycen@nori.com>
2022-05-18 18:19:57 -0700

    fix autofix issues

--- config/mocha.ts
+++ config/mocha.ts
@@ -1,7 +1,7 @@
 import type { HardhatUserConfig } from 'hardhat/types';
 
 const { CI } = process.env;
-console.log({ CI, tci: typeof CI });
+
 export const mocha: HardhatUserConfig['mocha'] = {
   parallel: !Boolean(CI),
 };
