120122db6d535d2cfd977be098990dba34efd732
rlfarman <rlfarman@gmail.com>
2022-06-07 15:11:04 -0700

    Fix solidity version

--- contracts/RemovalQueue.sol
+++ contracts/RemovalQueue.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity =0.8.13;
 import {RemovalUtils} from "./RemovalUtils.sol";
 import "solidity-linked-list/contracts/StructuredLinkedList.sol";
 
