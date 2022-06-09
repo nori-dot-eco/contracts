e962e643c46fc7d56f57222e4f134c601c8bf7b2
jaycenhorton <jaycen.horton@gmail.com>
2022-06-09 15:01:59 -0700

    add latest

--- config/mocha.ts
+++ config/mocha.ts
@@ -4,9 +4,10 @@ export const getMochaConfig = (
   environment = process.env
 ): HardhatUserConfig['mocha'] => {
   const { REPORT_GAS, CI } = environment;
-  return {
+  const mochaConfig: HardhatUserConfig['mocha'] = {
     parallel: !Boolean(REPORT_GAS) && !Boolean(CI),
   };
+  return mochaConfig;
 };
 
 export const mocha: HardhatUserConfig['mocha'] = getMochaConfig();
