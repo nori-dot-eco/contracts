aacd139fdadc77ab6696c545808bf85667559a4a
Jaycen Horton <jaycen.horton@gmail.com>
2022-06-13 12:53:16 -0700

    fix gas reports (#263)
    * fix gas reporter
    
    * undo environment loader changes
    
    * grab mocha config changes from jaycen-simulator
    
    * bump ci container size
    
    * bump ci container size
    
    * bump ci container size
    
    * bump ci container size
    
    * extend config for coverage
    
    * extend config for coverage
    
    * bump gasprice
    
    * tweak config
    
    * run tests sequentially in ci
    
    * add latest

--- tasks/coverage.ts
+++ tasks/coverage.ts
@@ -1,9 +1,8 @@
 import type { ActionType } from 'hardhat/config';
-import { task } from 'hardhat/config';
+import { extendConfig, task } from 'hardhat/config';
 import type { RunSuperFunction } from 'hardhat/types';
 
 import type { TestTaskOverrideParameters } from '@/tasks/test';
-import { validateTestEnvironment } from '@/tasks/utils/validate-environment';
 
 export const TASK = {
   name: 'coverage',
@@ -15,9 +14,20 @@ export const TASK = {
     hre: CustomHardHatRuntimeEnvironment,
     runSuper: RunSuperFunction<typeof taskArguments>
   ) => {
-    validateTestEnvironment({ reportGas: taskArguments?.reportGas });
     return runSuper(taskArguments);
   },
 } as const;
 
 task(TASK.name, TASK.description, TASK.run);
+
+extendConfig(() => {
+  // todo these would be set more appropriately in environment.ts
+  process.env.MINT = false;
+  process.env.FORCE_PROXY_DEPLOYMENT = true;
+  process.env.ETHERNAL = false;
+  process.env.TENDERLY = false;
+  process.env.NODE_ENV = 'test';
+  process.env.FIREBLOCKS_API_KEY = undefined;
+  process.env.FIREBLOCKS_API_SECRET_PATH = undefined;
+  process.env.FIREBLOCKS_VAULT_ID = undefined;
+});
--- tasks/test.ts
+++ tasks/test.ts
@@ -1,10 +1,7 @@
 import type { ActionType } from 'hardhat/config';
-import { task } from 'hardhat/config';
+import { extendConfig, task } from 'hardhat/config';
 import type { RunSuperFunction } from 'hardhat/types';
 import { TASK_TEST } from 'hardhat/builtin-tasks/task-names';
-
-import { getConfig } from '@/hardhat.config'; // eslint-disable-line import/extensions -- valid import extension
-import { validateTestEnvironment } from '@/tasks/utils/validate-environment';
 
 export interface TestTaskOverrideParameters {
   reportGas?: boolean;
@@ -20,8 +17,6 @@ export const TASK = {
     hre: CustomHardHatRuntimeEnvironment,
     runSuper: RunSuperFunction<typeof taskArguments>
   ) => {
-    validateTestEnvironment({ reportGas: taskArguments.reportGas });
-    (hre as any).userConfig = getConfig(process.env);
     return runSuper(taskArguments);
   },
 } as const;
@@ -30,3 +25,15 @@ task(TASK.name, TASK.description, TASK.run).addFlag(
   'reportGas',
   'Run tests and report gas usage'
 );
+
+extendConfig(() => {
+  // todo these would be set more appropriately in environment.ts
+  process.env.MINT = false;
+  process.env.FORCE_PROXY_DEPLOYMENT = true;
+  process.env.ETHERNAL = false;
+  process.env.TENDERLY = false;
+  process.env.NODE_ENV = 'test';
+  process.env.FIREBLOCKS_API_KEY = undefined;
+  process.env.FIREBLOCKS_API_SECRET_PATH = undefined;
+  process.env.FIREBLOCKS_VAULT_ID = undefined;
+});
