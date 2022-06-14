2c421ddbe33e2322e24722d2358ac35ae3d144f2
jaycenhorton <jaycen.horton@gmail.com>
2022-06-14 10:07:17 -0700

    add latest

--- test/Certificate.test.ts
+++ test/Certificate.test.ts
@@ -1,6 +1,6 @@
 // import { expect, setupTest } from '@/test/helpers';
 
-// todo test that it implements ERC1155PresetPausableNonTransferrable
+// todo test that it implements ERC1155PresetPausableNonTransferrable (perhaps by emitting event in mock?)
 
 // describe('Certificate', () => {
 //   describe('mintBatch', () => {
