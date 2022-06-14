c3efaa423d8d3a412af916355334f16eccf3e83b
jaycenhorton <jaycen.horton@gmail.com>
2022-06-14 07:59:22 -0700

    add latest

--- test/ERC1155PresetPausableNonTransferrable.test.ts
+++ test/ERC1155PresetPausableNonTransferrable.test.ts
@@ -58,6 +58,42 @@ describe('ERC1155PresetPausableNonTransferrable', () => {
       );
     });
   });
+  describe('safeBatchTransferFrom', () => {
+    it('should implement _beforeTokenTransfer', async () => {
+      const { mockERC1155PresetPausableNonTransferrable, hre } =
+        await setupTest({
+          userFixtures: {
+            buyer: {
+              mockERC1155PresetPausableNonTransferrableFixtures: {
+                tokens: [
+                  {
+                    to: global.hre.namedAccounts.buyer,
+                    removalId: 0,
+                    removalAmount: 1,
+                    data: createBatchMintData({ hre: global.hre, amount: 1 }),
+                  },
+                ],
+                approvalsForAll: [global.hre.namedAccounts.admin],
+              },
+            },
+          },
+        });
+      await expect(
+        mockERC1155PresetPausableNonTransferrable
+          .connect(hre.namedSigners.admin)
+          .safeBatchTransferFrom(
+            hre.namedAccounts.buyer,
+            hre.namedAccounts.investor1,
+            [0], // todo assign fixture before test and use values here
+            [1],
+            '0x'
+          )
+      ).to.emit(
+        mockERC1155PresetPausableNonTransferrable,
+        'BeforeTokenTransfer'
+      );
+    });
+  });
 
   // describe('_beforeTokenTransfer',()=>{ // MockRemoval exposes _beforeTokenTransfer as a public function
   //   it('doesnt allow minting when x')
