3717b58c29a911bee6bd367efe69952f10bfa5a7
jaycenhorton <jaycen.horton@gmail.com>
2022-06-28 15:42:35 -0700

    use _disableInitializer in constructors

--- contracts/ERC20Preset.sol
+++ contracts/ERC20Preset.sol
@@ -21,10 +21,6 @@ contract ERC20Preset is
    */
   bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-  constructor() {
-    _disableInitializers();
-  }
-
   /**
    * @notice Pauses all functions that can mutate state
    *
