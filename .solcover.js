bffec314d2242943ef011452c0975f114da3baf3
scottynomad <scott@nori.com>
2022-06-30 13:25:56 -0700

    Updated solcover config

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,11 @@
 module.exports = {
   skipFiles: [
-    'ScheduleTestHarness.sol',
-    'LockedNORI.sol',
     'deprecated/ERC777PresetPausablePermissioned.sol',
+    'test/ScheduleTestHarness.sol',
+    'test/LockedNORIV2Helper.sol',
+    'test/TestToken777.sol',
+    'test/DSTest/test.sol',
+    'mocks/MockCertificate.sol',
+    'mocks/MockERC1155PresetPausableNonTransferrable.sol'
   ],
 };
