bce6cbedefe82ad1d36ed03b0e4d5da4dde349fd
Jaycen Horton <jaycen.horton@gmail.com>
2022-07-04 15:33:13 -0700

    fix ability to run gas reporter locally (#281)

--- config/mocha.ts
+++ config/mocha.ts
@@ -3,14 +3,13 @@ import type { HardhatUserConfig } from 'hardhat/types';
 export const getMochaConfig = (
   environment = process.env
 ): HardhatUserConfig['mocha'] => {
-  const { REPORT_GAS, CI } = environment;
+  const { REPORT_GAS, CI, TRACE } = environment;
   const mochaConfig: HardhatUserConfig['mocha'] =
-    REPORT_GAS || CI
+    REPORT_GAS || CI || TRACE
       ? {}
       : {
-          parallel: !REPORT_GAS && !CI,
+          parallel: true,
         };
-
   return mochaConfig;
 };
 
