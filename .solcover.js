6e6b50013258ba0496cbba9ab26aff9c9cac5bed
jaycenhorton <jaycen.horton@gmail.com>
2022-07-06 00:40:00 -0700

    cleanup

--- .solcover.js
+++ .solcover.js
@@ -1,12 +1,4 @@
 module.exports = {
-  skipFiles: [
-    'deprecated/ERC777PresetPausablePermissioned.sol',
-    'test/ScheduleTestHarness.sol',
-    'test/LockedNORIV2Helper.sol',
-    'test/TestToken777.sol',
-    'test/DSTest/test.sol',
-    'mocks/MockCertificate.sol',
-    'mocks/MockERC1155PresetPausableNonTransferrable.sol'
-  ],
-  configureYulOptimizer: true
+  skipFiles: ['contracts/deprecated', 'contracts/test', 'lib', 'test'],
+  configureYulOptimizer: true,
 };
