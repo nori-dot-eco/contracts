b20f021decfbe72fa2912c7653f7c0aba902ff1c
rlfarman <rlfarman@gmail.com>
2022-08-02 15:07:05 -0700

    Remove unneeded events

--- contracts/ICertificate.sol
+++ contracts/ICertificate.sol
@@ -9,4 +9,16 @@ interface ICertificate {
     uint256[] removalIds,
     uint256[] removalAmounts
   );
+
+  /**
+   * @notice Emitted when a removal is released from a Certificate.
+   * @param certificatedId The certificate to connected to the removal.
+   * @param removalId The removal to update the balance for.
+   * @param amount The amount to added or removed for the certificate.
+   */
+  event RemovalReleased(
+    uint256 indexed certificatedId,
+    uint256 indexed removalId,
+    uint256 amount
+  );
 }
