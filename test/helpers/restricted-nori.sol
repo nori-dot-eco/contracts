65018cee67fc7980c0f5dabe34ef55899d13d258
jaycenhorton <jaycen.horton@gmail.com>
2022-08-02 21:25:48 -0700

    cleanup forge labels

--- test/helpers/restricted-nori.sol
+++ test/helpers/restricted-nori.sol
@@ -13,7 +13,7 @@ abstract contract UpgradeableRestrictedNORI is Upgradeable {
 
   function _deployRestrictedNORI() internal returns (RestrictedNORI) {
     RestrictedNORI impl = new RestrictedNORI();
-    vm.label(address(_rNori), "RestrictedNORI Implementation");
+    vm.label(address(impl), "RestrictedNORI Implementation");
     bytes memory initializer = abi.encodeWithSelector(impl.initialize.selector);
     RestrictedNORI rNoriProxy = RestrictedNORI(
       _deployProxy(address(impl), initializer)
