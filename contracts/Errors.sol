8794253e5a96f748b061bd0fac9d05d1127666ba
rlfarman <rlfarman@gmail.com>
2022-08-04 17:01:19 -0700

    Remove unused errors

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -3,11 +3,8 @@ pragma solidity =0.8.15;
 
 error ArrayLengthMismatch(string array1Name, string array2Name);
 error FunctionDisabled();
-error MissingRole(bytes32 role);
 error SenderNotRemovalContract();
-error RemovalAmountZero(uint256 tokenId);
 error NonexistentSchedule(uint256 scheduleId);
-error RecipientCannotBeZeroAddress();
 error InsufficientUnreleasedTokens(uint256 scheduleId);
 error InsufficientClaimableBalance(address account, uint256 scheduleId);
 error InvalidMinter(address account);
@@ -22,7 +19,6 @@ error InsufficientSupply();
 error UnauthorizedWithdrawal(); // todo consider allowing operators
 error OutOfStock();
 error LowSupplyAllowlistRequired();
-error RemovalNotInActiveSupply(uint256 removalId);
 error Unauthorized(); // todo perhaps use this for any authorization error
 error InvalidData();
 error InvalidCall();
