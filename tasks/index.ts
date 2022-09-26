8c63a493c81118aa583c53bb3a0bc5fc91f9f20c
amiecorso <amie@nori.com>
2022-09-26 15:42:10 -0700

    Revert "vesting task works with permit using mnemonic on local network"
    This reverts commit f33151ad9476a94387701306607bacf51034edb1.

--- tasks/index.ts
+++ tasks/index.ts
@@ -18,10 +18,10 @@ import { TASK as REMOVAL_TASK } from './removal';
 import { TASK as CERTIFICATE_TASK } from './certificate';
 import { TASK as MARKET_TASK } from './market';
 import { TASK as NORI_TASK } from './nori';
-import { TASK as LOCKED_NORI_TASK } from './locked-nori'; // todo make work with forked repo
+// import { TASK as LOCKED_NORI_TASK } from './locked-nori'; // todo make work with forked repo
 import { TASK as BRIDGED_POLYGON_NORI_TASK } from './bridged-polygon-nori';
 import { DEFENDER_ADD_TASK } from './defender';
-import { GET_VESTING_TASK as VESTING_TASK} from './vesting';  // todo make work with forked repo
+// import { GET_VESTING_TASK } from './vesting';  // todo make work with forked repo
 import { TASK as FORCE_UPGRADE_TASK } from './force-ugrade';
 import { TASK as SIGN_MESSAGE_TASK } from './sign-message';
 import { TASK as TEST_SIGN_TYPED_TASK } from './test-sign-typed';
@@ -69,10 +69,10 @@ export const TASKS = {
   [MARKET_TASK.name]: { ...MARKET_TASK },
   [REMOVAL_TASK.name]: { ...REMOVAL_TASK },
   [NORI_TASK.name]: { ...NORI_TASK },
-  [LOCKED_NORI_TASK.name]: { ...LOCKED_NORI_TASK },  // todo make work with forked repo
+  // [LOCKED_NORI_TASK.name]: { ...LOCKED_NORI_TASK },  // todo make work with forked repo
   [BRIDGED_POLYGON_NORI_TASK.name]: { ...BRIDGED_POLYGON_NORI_TASK },
   [DEFENDER_ADD_TASK.name]: { ...DEFENDER_ADD_TASK },
-  [VESTING_TASK.name]: { ...VESTING_TASK },  // todo make work with forked repo
+  // [VESTING_TASK.name]: { ...VESTING_TASK },  // todo make work with forked repo
   [FORCE_UPGRADE_TASK.name]: { ...FORCE_UPGRADE_TASK },
   [SIGN_MESSAGE_TASK.name]: { ...SIGN_MESSAGE_TASK },
   [TEST_SIGN_TYPED_TASK.name]: { ...TEST_SIGN_TYPED_TASK },
