513e82ec444f0f2bf7dbc6880d8f7ac8b5ec4548
Amie <corso.amie@gmail.com>
2022-11-10 08:25:22 -0800

    [NO-2376] M4 Prevent consigning from `Certificate` contract (#418)
    * prevent consigning from Certificate
    
    * add check for consigning from market too
    
    Co-authored-by: amiecorso <amie@nori.com>

--- contracts/Removal.sol
+++ contracts/Removal.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155Supp
 import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import "./Market.sol";
 import {RemovalIdLib, DecodedRemovalIdV0} from "./RemovalIdLib.sol";
-import {InvalidData, InvalidTokenTransfer, ForbiddenTransfer} from "./Errors.sol";
+import {InvalidData, InvalidTokenTransfer, ForbiddenTransfer, RemovalAlreadySoldOrConsigned} from "./Errors.sol";
 
 /**
  * @title An extended ERC1155 token contract for carbon removal accounting.
@@ -313,6 +313,9 @@ contract Removal is
     uint256 id,
     uint256 amount
   ) external whenNotPaused onlyRole(CONSIGNOR_ROLE) {
+    if (from == address(_certificate) || from == address(_market)) {
+      revert RemovalAlreadySoldOrConsigned({tokenId: id});
+    }
     _safeTransferFrom({
       from: from,
       to: address(_market),
