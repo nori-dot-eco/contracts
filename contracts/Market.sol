642ca088b90475ba91fcd595340b18b0a45059d1
rlfarman <rlfarman@gmail.com>
2022-11-29 12:48:06 -0800

    Remove log import

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155ReceiverUpgrad
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "hardhat/console.sol";
 
 import "./AccessPresetPausable.sol";
 import "./Certificate.sol";
