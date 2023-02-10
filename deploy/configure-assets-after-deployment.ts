3d7f24b5da6ade825ff1f56cc035c6ffdf00f88d
amiecorso <amie@nori.com>
2023-02-10 13:09:00 -0800

    successful Polygon deploy

--- deploy/configure-assets-after-deployment.ts
+++ deploy/configure-assets-after-deployment.ts
@@ -1,5 +1,6 @@
 import { Logger } from 'ethers/lib/utils';
 import type { DeployFunction } from 'hardhat-deploy/types';
+import type { ContractTransaction } from 'ethers';
 
 import {
   getBridgedPolygonNori,
@@ -9,6 +10,7 @@ import {
   getRestrictedNORI,
 } from '@/utils/contracts';
 
+const CONFIRMATIONS = 5;
 export const deploy: DeployFunction = async (environment) => {
   const hre = environment as unknown as CustomHardHatRuntimeEnvironment;
   Logger.setLogLevel(Logger.levels.DEBUG);
@@ -28,33 +30,66 @@ export const deploy: DeployFunction = async (environment) => {
   const rNori = await getRestrictedNORI({ hre, signer });
   const removal = await getRemoval({ hre, signer });
   const bpNori = await getBridgedPolygonNori({ hre, signer });
+  let txn: ContractTransaction;
   if (
     !(await rNori.hasRole(await rNori.SCHEDULE_CREATOR_ROLE(), removal.address))
   ) {
-    await rNori.grantRole(await rNori.SCHEDULE_CREATOR_ROLE(), removal.address);
+    hre.trace(
+      "Granting Removal the role 'SCHEDULE_CREATOR_ROLE' for RestrictedNORI..."
+    );
+    const txn = await rNori.grantRole(
+      await rNori.SCHEDULE_CREATOR_ROLE(),
+      removal.address
+    );
+    await txn.wait(CONFIRMATIONS);
     hre.trace(
       "Granted Removal the role 'SCHEDULE_CREATOR_ROLE' for RestrictedNORI"
     );
   }
 
   if (!(await rNori.hasRole(await rNori.MINTER_ROLE(), market.address))) {
-    await rNori.grantRole(hre.ethers.utils.id('MINTER_ROLE'), market.address);
+    hre.trace("Granting Market the role 'MINTER_ROLE' for RestrictedNORI...");
+    txn = await rNori.grantRole(
+      hre.ethers.utils.id('MINTER_ROLE'),
+      market.address
+    );
+    await txn.wait(CONFIRMATIONS);
+    hre.trace("Granted Market the role 'MINTER_ROLE' for RestrictedNORI");
   }
-  hre.trace("Granted Market the role 'MINTER_ROLE' for RestrictedNORI");
-  await rNori.registerContractAddresses(
+  // TODO figure out how to make a check about what these addresses are currently set to
+  // bigger TODO: expose getters for these on the contract
+  txn = await rNori.registerContractAddresses(
     bpNori.address,
     removal.address,
     market.address
   );
+  await txn.wait(CONFIRMATIONS);
   hre.trace('Set removal, bpNori and market addresses in rNori');
-  await certificate.registerContractAddresses(removal.address);
-  hre.trace('Set removal addresses in rNori');
-  await removal.registerContractAddresses(market.address, certificate.address);
-  hre.trace('Set market and certificate addresses in Removal');
+  if ((await certificate.getRemovalAddress()) !== removal.address) {
+    hre.trace('Setting removal address in Certificate contract...');
+    txn = await certificate.registerContractAddresses(removal.address);
+    await txn.wait(CONFIRMATIONS);
+    hre.trace('Set removal addresses in Certificate');
+  }
+  if (
+    (await market.getRemovalAddress()) !== removal.address ||
+    (await market.getCertificateAddress()) !== certificate.address
+  ) {
+    hre.trace(
+      'Setting removal and certificate addresses in Removal contract...'
+    );
+    txn = await removal.registerContractAddresses(
+      market.address,
+      certificate.address
+    );
+    await txn.wait(CONFIRMATIONS);
+    hre.trace('Set market and certificate addresses in Removal');
+  }
 };
 
 export default deploy;
 deploy.tags = ['market', 'configure'];
+// TODO is there a way to remove this 'Market' dependency?
 deploy.dependencies = ['Market'];
 deploy.skip = async (hre) =>
   Promise.resolve(
