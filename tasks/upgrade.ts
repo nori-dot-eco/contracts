3faf156c06c86b8e0774f9becf48fa8b8c7701bc
jaycenhorton <jaycen.horton@gmail.com>
2023-03-01 17:21:28 -0800

    add flag for validation

--- tasks/upgrade.ts
+++ tasks/upgrade.ts
@@ -11,27 +11,36 @@ export const TASK = {
   name: 'upgrade',
   description: 'Interact with upgradeable contracts',
   run: async (
-    { contractNames }: { contractNames: (keyof Contracts)[] },
+    {
+      contractNames,
+      validate,
+    }: { contractNames: (keyof Contracts)[]; validate: boolean },
     hre: CustomHardHatRuntimeEnvironment
   ): Promise<void> => {
-    const [signer] = await hre.getSigners();
-    for (const name of contractNames) {
-      const proxy = await hre.deployments.get(name);
-      const newImplementation = await hre.ethers.getContractFactory(
-        name,
-        signer
-      );
-      await hre.upgrades.validateUpgrade(proxy, newImplementation, {
-        unsafeAllow: ['delegatecall'],
-      });
+    if (validate) {
+      const [signer] = await hre.getSigners();
+      for (const name of contractNames) {
+        const proxy = await hre.deployments.get(name);
+        const newImplementation = await hre.ethers.getContractFactory(
+          name,
+          signer
+        );
+        await hre.upgrades.validateUpgrade(proxy, newImplementation, {
+          unsafeAllow: ['delegatecall'],
+        });
+      }
+    } else {
+      hre.log('Skipping validation. No other options available yet.');
     }
   },
 } as const;
 
-task(TASK.name, TASK.description, TASK.run).addVariadicPositionalParam(
-  'contractNames',
-  'The upgradeable contract names',
-  undefined,
-  undefined,
-  false
-);
+task(TASK.name, TASK.description, TASK.run)
+  .addFlag('validate')
+  .addVariadicPositionalParam(
+    'contractNames',
+    'The upgradeable contract names',
+    undefined,
+    undefined,
+    false
+  );
