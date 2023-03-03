9ae8a433bb0328a0aa77cf6ff0bab3e87657d22a
jaycenhorton <jaycen.horton@gmail.com>
2023-03-02 22:37:58 -0800

    add custom eslint action

--- utils/math.ts
+++ utils/math.ts
@@ -3,6 +3,6 @@ import type { BigNumber } from 'ethers';
 import { Zero } from '../constants/units';
 
 /** Calculates the sum of an array of bn.js BigNumbers */
-export const sum = (numbers: BigNumber[]): BigNumber => {
+ export const sum = (numbers: BigNumber[]): BigNumber => {
   return numbers.reduce((total, removal) => total.add(removal), Zero);
 };
