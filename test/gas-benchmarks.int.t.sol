0e2b7af74539934ef2d8b326e6e9b313d8a22339
amiecorso <amie@nori.com>
2024-02-20 15:21:29 -0800

    add small order benchmark

--- test/gas-benchmarks.int.t.sol
+++ test/gas-benchmarks.int.t.sol
@@ -111,3 +111,58 @@ contract GasBenchmark_buyingFromManyRemovals_singleSupplier is GasBenchmark {
     // );
   }
 }
+
+contract GasBenchmark_buyingFromOneRemoval_singleSupplier is GasBenchmark {
+  uint256 numberOfRemovals;
+
+  function setUp() external {
+    numberOfRemovals = 1;
+    _removalIdArray = new uint256[](numberOfRemovals);
+    for (uint256 i = 0; i < numberOfRemovals; i++) {
+      _removalIdArray[i] = _seedAndListRemoval({
+        supplier: _namedAccounts.supplier,
+        amount: 1 ether,
+        vintage: uint16(2018 + (i % 4))
+      });
+    }
+
+    assertEq(
+      _removal.balanceOfBatch(
+        new address[](_removalIdArray.length).fill(address(_market)),
+        _removalIdArray
+      ),
+      new uint256[](_removalIdArray.length).fill(1 ether),
+      "Expected the market to own the removals"
+    );
+    assertEq(_removal.getMarketBalance(), numberOfRemovals * 1 ether);
+    assertEq(
+      _removal.numberOfTokensOwnedByAddress(address(_market)),
+      numberOfRemovals
+    );
+    _market.grantRole({
+      role: _market.MARKET_ADMIN_ROLE(),
+      account: _namedAccounts.buyer
+    });
+    _assertExpectedBalances(_namedAccounts.supplier, 0, false, 0);
+    _assertExpectedBalances(address(_certificate), 0, false, 0);
+    assertEq(_removal.balanceOf(address(_certificate), _removalIdArray[0]), 0);
+    assertEq(
+      _certificate.getPurchaseAmount(_certificateTokenId),
+      0,
+      "Certificate balance is wrong"
+    );
+  }
+
+  function test() external {
+    vm.prank(_namedAccounts.buyer);
+    _market.swapWithoutFeeSpecialOrder({
+      recipient: _namedAccounts.buyer,
+      purchaser: _namedAccounts.buyer,
+      amount: 0.0001 ether,
+      customFee: 0,
+      customPriceMultiple: 0,
+      supplier: _namedAccounts.supplier,
+      vintages: new uint256[](0)
+    });
+  }
+}
